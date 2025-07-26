48fc1c434f8ab4834377da75ef86c3d3acca4d74
SanyaTheGreat <polikalexander@gmail.com>
2025-07-26 04:12:39 +0300

    wd4

--- src/controllers/users/withdrawReferral.js
+++ src/controllers/users/withdrawReferral.js
@@ -20,7 +20,8 @@ async function initProjectWallet() {
   console.log('walletKey.secretKey instanceof Uint8Array:', walletKey.secretKey instanceof Uint8Array);
 
   // walletId с типом bigint
-  const walletId = 0;
+  const walletId = 0n;
+  
   console.log('typeof walletId:', typeof walletId);
   console.log('walletId:', walletId);
 
